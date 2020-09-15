7e9ccee59b5dd4c967bc0f913d9422cdd15d52de
MicaelaStempellato <micastempe@gmail.com>
2020-09-15 16:45:01 -0300

    agrego la carpeta js y actualizo home

---
+++ SITIO/js/home_cursos.js
@@ -0,0 +1,66 @@
+$("#boton2").on('click', function(){
+  
+    $("#titulo_experiencia").fadeToggle();
+    $("#por_experiencia").fadeToggle();
+  
+  $("#titulo_edad").hide();
+  $("#por_edad").hide();
+
+	
+  });
+	
+
+$("#boton1").on('click', function(){
+  
+    $("#titulo_ambiente").fadeToggle();
+    $("#por_ambiente").fadeToggle();
+  
+   $("#titulo_edad").hide();
+   $("#por_edad").hide();
+
+	
+  });
+
+$("#boton3").on('click', function(){
+  
+    $("#titulo_edad").fadeToggle();
+    $("#por_edad").fadeToggle();
+  
+   $("#titulo_experiencia").hide();
+   $("#por_experiencia").hide();
+
+	
+  });
+
+  $("#boton4").on('click', function(){
+  
+    $("#titulo_ambiente").fadeToggle();
+    $("#por_ambiente").fadeToggle();
+  
+   $("#titulo_experiencia").hide();
+   $("#por_experiencia").hide();
+  
+	
+  });
+  
+  $("#boton5").on('click', function(){
+  
+    $("#titulo_experiencia").fadeToggle();
+    $("#por_experiencia").fadeToggle();
+  
+   $("#titulo_ambiente").hide();
+   $("#por_ambiente").hide();
+
+	
+  });
+
+  $("#boton6").on('click', function(){
+  
+    $("#titulo_edad").fadeToggle();
+    $("#por_edad").fadeToggle();
+  
+   $("#titulo_ambiente").hide();
+   $("#por_ambiente").hide();
+  
+	
+  });
