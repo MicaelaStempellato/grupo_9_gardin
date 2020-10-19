046db06b06719f7873ab44bdc651f1b99a40deba
MicaelaStempellato <micastempe@gmail.com>
2020-10-19 19:22:41 -0300

    agrego validaciones de crear curso

--- digital-gardin/src/controllers/productsController.js
+++ digital-gardin/src/controllers/productsController.js
@@ -1,5 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const { validationResult } = require('express-validator');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 module.exports = {
     create: function(req, res, next) {
         res.render('products/createForm', { title: 'Crear Curso', css: 'crearCurso' });
-      }
+      },
+      edit: function(req, res, next) {
+        res.render('products/editForm', { title: 'Editar Curso', css: 'crearCurso' });
+      },
+      store: function(req, res, next) {
+        let errors = validationResult(req);
+		if(errors.isEmpty()){
+
+            let elId;
+            if(products==""){
+                elId = 1
+            } else{
+                elId = products[products.length-1].id+1
+            };
+
+		let newProduct = {
+			id: elId,
+			...req.body,
+			image: req.file.filename
+		}
+		let newDB = [...products, newProduct];
+		let newDBJSON = JSON.stringify(newDB, null, 2);
+		fs.writeFileSync(productsFilePath, newDBJSON)
+
+		res.redirect('/');
+
+		} else {
+			return res.render('products/createForm', {errors: errors.errors, old: req.body, title: 'Editar Curso', css: 'crearCurso'})
+		}
+	}
+
+
 }
--- digital-gardin/src/routes/create.js
+++ digital-gardin/src/routes/create.js
@@ -1,8 +1,41 @@
 var express = require('express');
 var router = express.Router();
 let productsController = require('../controllers/productsController')
+const multer = require('multer');
+const path = require('path');
+let productValidator = require('../middlewares/productValidator')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/images/cursos'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now()+path.extname(file.originalname))
+    }
+  })
+   
+  var upload = multer({
+    storage,
+ 
+    // Validate image
+    fileFilter: (req, file, cb) => {
+ 
+       const acceptedExtensions = ['.jpg', '.jpeg', '.png', '.JPG', '.JPEG', '.PNG'];
+ 
+       const ext = path.extname(file.originalname);
+       
+       if (!acceptedExtensions.includes(ext)) {
+          req.file = file;
+       }
+ 
+       cb(null, acceptedExtensions.includes(ext));
+    }
+ });
+
 
 /* GET pcreate form page. */
 router.get('/', productsController.create);
+router.get('/edit', productsController.edit);
+router.post('/', upload.single('image'), productValidator.product, productsController.store);
 
 module.exports = router;
