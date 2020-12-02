0a188ddf67bd479c9ec4a697f4c7eb7c6320de10
Patricio <patriciomqcontreras@gmail.com>
2020-12-02 17:34:41 -0300

    Agrego los modelos "useres" y "cathegory"

---
+++ digital-gardin/src/database/models/Category.js
@@ -0,0 +1,12 @@
+const {sequelize, DataTypes} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    const category = sequelize.define('Category', {
+        category_name: DataTypes.STRING
+    })
+    category.associate = (models => {
+        category.hasMany(models.User, {
+            as: 'user',
+            foreignKey: 'category_id'
+        });
+    })
+}
---
+++ digital-gardin/src/database/models/User.js
@@ -0,0 +1,17 @@
+const {sequelize, DataTypes} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    const user = sequelize.define('User', {
+        first_name: DataTypes.STRING,
+        last_name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        avatar: DataTypes.STRING,
+        category_id: DataTypes.INTEGER
+    })
+    user.associate = (models => {
+        user.belongsTo(models.Category, {
+            as: 'category',
+            foreignKey: 'category_id'
+        });
+    })
+}
