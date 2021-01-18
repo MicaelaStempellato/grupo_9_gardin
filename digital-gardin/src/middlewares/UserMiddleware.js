288292f3e0b34b40e1057995edaba803555964e2
Patricio <patriciomqcontreras@gmail.com>
2021-01-18 17:07:02 -0300

    ahora users e invitados no pueden acceder a rutas que no deberían

---
+++ digital-gardin/src/middlewares/UserMiddleware.js
@@ -0,0 +1,9 @@
+const db = require('../database/models');
+
+module.exports = function(req, res, next){
+    if(req.session.userLog != undefined ){
+        next();
+    }else{
+        res.redirect('/users/login');
+    }
+}
--- digital-gardin/src/routes/users.js
+++ digital-gardin/src/routes/users.js
@@ -4,6 +4,8 @@ let usersController = require('../controllers/usersController')
 let usersValidator = require ('../middlewares/usersValidator')
 const multer = require('multer');
 const path = require('path');
+const usersOnly = require('../middlewares/UserMiddleware');
+const guestsOnly = require('../middlewares/guestMiddleware');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -33,15 +35,15 @@ var upload = multer({
  });
 
 // LogIn de usuarios.
-router.get('/login', usersController.login);
+router.get('/login', guestsOnly, usersController.login);
 router.post('/login', usersValidator.usersLog, usersController.loginForm);
 
 //Registración de usuarios
-router.get('/signin', usersController.registrarse);
+router.get('/signin', guestsOnly, usersController.registrarse);
 router.post('/registro', usersValidator.usersReg, usersController.registroForm);
 
 //Modificación de usuarios
-router.get('/edit', usersController.edit)
+router.get('/edit', usersOnly, usersController.edit)
 router.post('/edit', usersValidator.userEdit, usersController.editForm)
 router.post('/avatar', upload.single('avatar'), usersValidator.avatar, usersController.editAvatar)
 router.post('/password', usersValidator.passEdit, usersController.editPass)
@@ -49,6 +51,6 @@ router.post('/password', usersValidator.passEdit, usersController.editPass)
 router.post('/logout', usersController.logout)
 
 /* GET users profile. */
-router.get('/profile', usersController.perfil);
+router.get('/profile', usersOnly, usersController.perfil);
 
 module.exports = router;
