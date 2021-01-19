ed2df7814d765cb83da3064ed01c9332db2b106a
MicaelaStempellato <micastempe@gmail.com>
2021-01-19 18:19:26 -0300

    subo api de products y product detail

--- digital-gardin/src/controllers/api/apiController.js
+++ digital-gardin/src/controllers/api/apiController.js
@@ -4,6 +4,10 @@ module.exports = {
         try{
             let respuesta
             const products = await Product.findAll({include:['edad', 'experiencia', 'ambiente', 'requisitos', 'unidades']})
+            const prods = products.map(producto=>{
+                let datav = producto.dataValues;
+               datav.detail = `/api/products/${datav.id}`       
+            })
             if(products.length > 0){
                 respuesta = {
                     metadata: {
@@ -19,5 +23,32 @@ module.exports = {
             console.log(error)
             res.render('error')
         }
-    }
+    },
+
+    detail: async(req, res) => {
+        try{
+            let respuesta
+            const products = await Product.findByPk( req.params.id, {include:['edad', 'experiencia', 'ambiente', 'requisitos', 'unidades']})
+            
+            console.log(products);
+                let datav = products.dataValues;
+               datav.link_img = `/images/cursos/${datav.image}`   
+               
+               console.log(products);
+           
+            if(Object.keys(products).length > 0){
+                respuesta = {
+                    metadata: {
+                        status: 200,
+                    },
+                    resultados: products
+                }
+        }
+        res.json(respuesta)
+            
+        }catch(error){
+            console.log(error)
+            res.render('error')
+        }
+    },
 }
--- digital-gardin/src/routes/api/productsApi.js
+++ digital-gardin/src/routes/api/productsApi.js
@@ -3,5 +3,6 @@ var router = express.Router();
 let apiController = require('../../controllers/api/apiController');
 
 router.get('/products', apiController.all);
+router.get('/products/:id', apiController.detail);
 
 module.exports = router;
